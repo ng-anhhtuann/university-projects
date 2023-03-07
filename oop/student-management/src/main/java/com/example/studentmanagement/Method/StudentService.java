cc13c5e79366e91a323408c3a88839c4ee7a8415
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-08 01:50:48 +0700

    Mapping actions to url

--- oop/student-management/src/main/java/com/example/studentmanagement/Method/StudentService.java
+++ oop/student-management/src/main/java/com/example/studentmanagement/Method/StudentService.java
@@ -1,2 +1,22 @@
-package com.example.studentmanagement.Method;public class StudentService {
+package com.example.studentmanagement.Method;
+
+import com.example.studentmanagement.Model.Student;
+import com.example.studentmanagement.Repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/")
+public class StudentService {
+    private final StudentRepository studentRepository = StudentRepository.getStudentRepository();
+
+    @PostMapping(value="insert")
+    public Object insertStudent(@RequestBody Student student){
+        return studentRepository.postStudent(student);
+    }
+
+    @DeleteMapping(value="delete-by-id")
+    public Object deleteById(@RequestBody int studentId){
+        return studentRepository.deleteStudentByStudentId(studentId);
+    }
+
 }
