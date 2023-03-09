0f43c85c573032a4215f62142c9912cf2312c191
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-09 14:17:12 +0700

    Add annotation OpenAPI

--- oop/student-management/src/main/java/com/example/studentmanagement/StudentManagementApplication.java
+++ oop/student-management/src/main/java/com/example/studentmanagement/StudentManagementApplication.java
@@ -1,14 +1,15 @@
 package com.example.studentmanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableWebMvc
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Student Management API", version = "1.0"))
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
