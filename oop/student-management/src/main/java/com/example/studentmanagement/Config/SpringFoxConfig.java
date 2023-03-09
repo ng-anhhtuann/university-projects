490e971bceac811d418e38097df3218d2b7156fe
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-09 14:17:28 +0700

    Config swagger

---
+++ oop/student-management/src/main/java/com/example/studentmanagement/Config/SpringFoxConfig.java
@@ -0,0 +1,36 @@
+package com.example.studentmanagement.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+            "Student Management",
+            "Object Oriented Programming",
+            "1.0",
+            "spring.io",
+            new Contact("Nguyen Anh Tuan", "github.com/ng-anhhtuann","nguyenanhtuan1232@gmail.com"),
+            "License of API", "github.com/ng-anhhtuann", Collections.emptyList());
+    }
+}
