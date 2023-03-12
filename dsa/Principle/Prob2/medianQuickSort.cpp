5f8a73ef62cbd42f58669de3ae795a1aa17cbb66
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-12 20:16:12 +0700

    Heap sort impl

--- dsa/Principle/Prob2/medianQuickSort.cpp
+++ dsa/Principle/Prob2/medianQuickSort.cpp
@@ -17,12 +17,17 @@ void Swap(int &a,int &b){
     b = tmp;
 }
 
-void QuickSort(int a[], int left, int right)
+void QuickSort(int a[], int l, int r)
 { 	
 	int	i, j;
-	if (left >= right)	return;
-  	double x = (a[left] + a[right])/2; 
-  	i = left; j = right;
+	if (l >= r)	return;
+  	double x = (a[l] + a[r])/2; 
+    /* another approach:
+    x = a((l+r)/2) 
+    x = a[l]
+    x = a[r]
+    */
+  	i = l; j = r;
   	 do{
      	while(a[i] < x) i++;
      	while(a[j] > x) j--;
@@ -31,27 +36,48 @@ void QuickSort(int a[], int left, int right)
        	i++ ; j--;
 	 	}
 	} while(i < j) ;
-	if(left<j) QuickSort(a, left, j);
-	if(i<right) QuickSort(a, i, right);
+	if(l<j) QuickSort(a, l, j);
+	if(i<r) QuickSort(a, i, r);
+}
+void heapify(int a[], int n, int start){
+    int l = start*2+1;
+    int r = start*2+2;
+    int large = start;
+    if ( l < n && a[l] > a[large]) large = l;
+    if ( r < n && a[r] > a[large]) large = r;
+    if (large != start){
+        Swap(a[large], a[start]);
+        heapify(a,n,large);
+    }
+}
+void heap( int a[], int n){
+    for ( int i = n/2-1; i >= 0; i--){
+        heapify(a,n,i);
+    }
+    for ( int i = n-1; i >= 0; i--){
+        Swap(a[i], a[0]);
+        heapify(a,i,0);
+    }
 }
 int main() {
     int n = 10;
 
-    int a[] = {10, 17, 9, -33, 28, 4, 35, -10, 38, 7};//Random Array
+    int a[] = {43, 44, 9, -25, 28, 3, 32, -10, 10, 0};//Random Array
     
-    int b[] = {10, 9, 7, 5, 3, 1, -3, -5, -8, -17}; //Descending Sorted Array
+    // int b[] = {10, 9, 7, 5, 3, 1, -3, -5, -8, -17}; //Descending Sorted Array
 
     cout << "Array A before sort\n";
     log(a,n);
-    cout << "Array A after quicksort: \n";
-    QuickSort(a,0,n-1);
+    cout << "Array A after sort: \n";
+    // QuickSort(a,0,n-1);
+    heap(a, n);
     log(a,n);
 
-    cout << "Array B before sort\n";
-    log(b,n);
-    cout << "Array B after quicksort: \n";
-    QuickSort(b,0,n-1);
-    log(b,n);
+    // cout << "Array B before sort\n";
+    // log(b,n);
+    // cout << "Array B after quicksort: \n";
+    // QuickSort(b,0,n-1);
+    // log(b,n);
 
     return 0;
 }
