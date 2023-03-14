708b1db653909dbdd685d3326209821e06ed751d
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-14 22:19:32 +0700

    Revert config

--- src/main/java/com/example/studentmanagement/StudentManagementApplication.java
+++ src/main/java/com/example/studentmanagement/StudentManagementApplication.java
@@ -5,10 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableWebMvc
@@ -17,19 +14,6 @@ public class StudentManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedOrigins("*")
-					.allowedMethods("*")
-					.allowedHeaders("*");
-			}
-		};
 	}
 
 }
