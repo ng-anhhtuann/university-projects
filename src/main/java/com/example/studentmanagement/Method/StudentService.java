a714eb35697f57dc99c13471dff7ce4bf7804239
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-16 16:36:51 +0700

    Change Path to Param

--- src/main/java/com/example/studentmanagement/Method/StudentService.java
+++ src/main/java/com/example/studentmanagement/Method/StudentService.java
@@ -39,19 +39,19 @@ public class StudentService implements IService {
 
     @Override
     @GetMapping(value="get-faculty")
-    public Object facultyStudent(@PathVariable String faculty){
+    public Object facultyStudent(@RequestParam String faculty){
         return studentRepository.getStudentsFromFaculty(faculty);
     }
 
     @Override
     @GetMapping(value="get-class")
-    public Object classStudent(@PathVariable String classRoom){
+    public Object classStudent(@RequestParam String classRoom){
         return studentRepository.getStudentsFromClass(classRoom);
     }
 
     @Override
     @GetMapping(value="get-student-id")
-    public Object studentById(@PathVariable Map<String, Integer> req){
+    public Object studentById(@RequestParam Map<String, Integer> req){
         return studentRepository.getStudentFromStudentId(req);
     }
 
