580500431bb03f30271067cdaede220786fc0d91
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-16 22:04:19 +0700

    Delete value prop

--- oop/student-management-fe/src/shared/Input.js
+++ oop/student-management-fe/src/shared/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../App.css';
 
-const Input = ({ label, name, type, value, onChange }) => {
+const Input = ({ label, name, type, onChange }) => {
   return (
     <div className='between'>
       <label htmlFor={name}>{label}:</label>
-      <input type={type} name={name} id={name} value={value} onChange={onChange} className="Input" required={true} />
+      <input type={type} name={name} id={name} onChange={onChange} className="Input" required={true} />
     </div>
   );
 };
