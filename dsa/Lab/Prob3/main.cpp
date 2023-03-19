dbcf6c48448f2965c376026760983901567cd2dc
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-19 16:43:28 +0700

    Add doc and code for lab2

--- dsa/Lab/Prob3/main.cpp
+++ dsa/Lab/Prob3/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+
 using namespace std;
 
 struct Node {
@@ -44,6 +48,27 @@ void bubbleSort(Node* head) {
     } while (swapped);
 }
 
+void selectionSort(Node **head) {
+    Node *nodeA = *head, *nodeB, *min;
+    if (nodeA == NULL)
+        return;
+    while (nodeA->next != NULL) {
+        min = nodeA;
+        nodeB = nodeA->next;
+        while (nodeB != NULL) {
+            if (nodeB->data < min->data)
+                min = nodeB;
+            nodeB = nodeB->next;
+        }
+        if (min != nodeA) {
+            int t = min->data;
+            min->data = nodeA->data;
+            nodeA->data = t;
+        }
+        nodeA = nodeA->next;
+    }
+}
+
 void insertNodeAtTail(Node** head, int data) {
     Node* newNode = new Node();
     newNode->data = data;
@@ -73,28 +98,102 @@ void insertNodeAtIndex(Node** head, int index, int data) {
     currentNode->next = newNode;
 }
 
-void printList(Node* node) {
-    while (node) {
-        cout << node->data << " ";
-        node = node->next;
+Node* getLastNode(Node* head) {
+    Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    return current;
+}
+
+void swap(Node* a, Node* b) {
+    int temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+
+Node* partition(Node* head, Node* end, Node** newHead, Node** newEnd) {
+    Node* pivot = end;
+    Node* prev = NULL, *current = head, *tail = pivot;
+
+    while (current != pivot) {
+        if (current->data < pivot->data) {
+            if (*newHead == NULL) {
+                *newHead = current;
+            }
+            prev = current;
+            current = current->next;
+        }
+        else {
+            if (prev) {
+                prev->next = current->next;
+            }
+            Node* temp = current->next;
+            current->next = NULL;
+            tail->next = current;
+            tail = current;
+            current = temp;
+        }
+    }
+
+    if (*newHead == NULL) {
+        *newHead = pivot;
+    }
+    *newEnd = tail;
+
+    return pivot;
+}
+
+void quickSort(Node** head, Node* end) {
+    if (*head == NULL || *head == end) {
+        return;
+    }
+
+    Node* newHead = NULL, *newEnd = NULL;
+    Node* pivot = partition(*head, end, &newHead, &newEnd);
+
+    if (newHead != pivot) {
+        Node* temp = newHead;
+        while (temp->next != pivot) {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        quickSort(&newHead, temp);
+        temp = getLastNode(newHead);
+        temp->next = pivot;
+    }
+    quickSort(&pivot->next, newEnd);
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->next;
     }
     cout << endl;
 }
 
+void push(Node** head, int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+
 int main() {
     Node* head = NULL;
-    insertNodeAtTail(&head, 5);
-    insertNodeAtTail(&head, 3);
-    insertNodeAtTail(&head, 8);
-    insertNodeAtTail(&head, 1);
-    insertNodeAtTail(&head, 6);
+    push(&head, 5);
+    push(&head, 20);
+    push(&head, 4);
+    push(&head, 3);
+    push(&head, 30);
 
-    cout << "Original List: ";
+    cout << "Original Linked List: ";
     printList(head);
+    quickSort(&head, getLastNode(head));
 
-    bubbleSort(head);
-
-    cout << "Sorted List: ";
+    cout << "Sorted Linked List: ";
     printList(head);
 
     return 0;
