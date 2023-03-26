154b77bd4b65c530c9ec32675c95128edd116520
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-03-26 19:31:38 +0700

    Create tables

--- db/CreateTables.sql
+++ db/CreateTables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Sach (
-    MaSach INT PRIMARY KEY,
+    MaSach INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     TenSach VARCHAR(255),
     TacGia VARCHAR(255),
     NhaXuatBan VARCHAR(255),
-    NgayXuatBan DATE,
+    GiaNhap FLOAT,
     GiaBan FLOAT,
     SoLuongTon INT
 );
@@ -15,30 +15,41 @@ CREATE TABLE KhachHang (
     SoDienThoai VARCHAR(20)
 );
 CREATE TABLE NhanVien (
-    MaNhanVien INT PRIMARY KEY,
+    MaNhanVien INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     TenNhanVien VARCHAR(255),
     DiaChi VARCHAR(255),
     Email VARCHAR(255),
     SoDienThoai VARCHAR(20),
     ChucVu VARCHAR(255)
 );
-CREATE TABLE KhoSach (
-    MaSach INT,
-    ViTri VARCHAR(255),
-    PRIMARY KEY (MaSach, ViTri),
-    FOREIGN KEY (MaSach) REFERENCES Sach(MaSach)
-);
-CREATE TABLE NhaCungCap (
-    MaNhaCungCap INT PRIMARY KEY,
-    TenNhaCungCap VARCHAR(255),
-    DiaChi VARCHAR(255),
-    Email VARCHAR(255),
-    SoDienThoai VARCHAR(20)
+CREATE TABLE NhapKho (
+    MaNhapKho INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    NgayNhapKho DATE,
+    ThanhTien FLOAT
 );
 CREATE TABLE HoaDon (
-    MaHoaDon INT PRIMARY KEY,
+    MaHoaDon INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     MaKhachHang INT,
     NgayLap DATE,
     TongTien FLOAT,
+    MaNhanVien INT,
     FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang)
 );
+CREATE TABLE ChiTietNhapKho (
+    MaNhapKho INT NOT NULL,
+    MaSach INT,
+    SoLuong INT,
+    DonGia FLOAT,
+    ThanhTien FLOAT,
+    FOREIGN KEY (MaSach) REFERENCES Sach(MaSach),
+    FOREIGN KEY (MaNhapKho) REFERENCES NhapKho(MaNhapKho)
+);
+CREATE TABLE ChiTietHoaDon (
+    MaHoaDon INT NOT NULL,
+    MaSach INT,
+    SoLuong INT,
+    ThanhTien FLOAT,
+    FOREIGN KEY (MaHoaDon) REFERENCES HoaDon(MaHoaDon),
+    FOREIGN KEY (MaSach) REFERENCES Sach(MaSach)
+);
+
