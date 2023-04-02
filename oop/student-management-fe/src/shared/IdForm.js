5e86fbc8d35a45e09b8fe49ad44f045684d783a2
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-04-03 01:08:01 +0700

    Delete value property

--- oop/student-management-fe/src/shared/IdForm.js
+++ oop/student-management-fe/src/shared/IdForm.js
@@ -3,10 +3,10 @@ import Input from "./Input";
 import '../App.css';
 import Submit from "./Submit";
 
-export default function IdForm({title,onChange, onSubmit, hide, type, name, value}){
+export default function IdForm({title,onChange, onSubmit, hide, type, name}){
     return hide ? <></> : (
         <div className="inputWidth">
-            <Input label={title} name={name} type={type} value={value} onChange={onChange} />
+            <Input label={title} name={name} type={type} onChange={onChange} />
             <div className='middlePos'>
                 <Submit onClick={onSubmit}/>
             </div>
--- oop/student-management-fe/src/shared/UpdateForm.js
+++ oop/student-management-fe/src/shared/UpdateForm.js
@@ -3,11 +3,11 @@ import Input from "./Input";
 import '../App.css';
 import Submit from "./Submit";
 
-export default function UpdateForm({title,onChangeId,onChange, onSubmit, hide, type, name, value}){
+export default function UpdateForm({title,onChangeId,onChange, onSubmit, hide, type, name}){
     return hide ? <></> : (
         <div className="inputWidth">
-            <Input label={"Student ID"} name={"studentId"} type={"number"} value={undefined} onChange={onChangeId} />
-            <Input label={title} name={name} type={type} value={value} onChange={onChange} />
+            <Input label={"Student ID"} name={"studentId"} type={"number"} onChange={onChangeId} />
+            <Input label={title} name={name} type={type} onChange={onChange} />
             <div className='middlePos'>
                 <Submit onClick={onSubmit}/>
             </div>
