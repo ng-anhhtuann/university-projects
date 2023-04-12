79de08adb223bf019048904bc24a99f278d891ee
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-04-12 22:19:06 +0700

    Reuse method get res from future

--- oop/student-management/src/main/java/com/example/studentmanagement/Utils/Utilities.java
+++ oop/student-management/src/main/java/com/example/studentmanagement/Utils/Utilities.java
@@ -1,5 +1,20 @@
 package com.example.studentmanagement.Utils;
 
+import com.example.studentmanagement.Model.Response;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class Utilities {
     public static final String DEFAULT = "default";
+
+    public static Object getResultFromFuture(Future<Object> future) {
+        Object result = null;
+        try {
+            result = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            return new Response(false,"Something's wrong");
+        }
+        return result;
+    }
 }
