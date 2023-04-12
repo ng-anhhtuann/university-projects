3dc72b1a361ea70d3887148c470332eeb10ae5ec
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-04-12 22:19:38 +0700

    Multi-threading base on concurency req

--- oop/student-management/src/main/java/com/example/studentmanagement/Method/StudentService.java
+++ oop/student-management/src/main/java/com/example/studentmanagement/Method/StudentService.java
@@ -8,70 +8,86 @@ import com.example.studentmanagement.Repository.StudentRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static com.example.studentmanagement.Utils.Utilities.getResultFromFuture;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value="/")
 public class StudentService implements IStudent {
     private final StudentRepository studentRepository = StudentRepository.getStudentRepository();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(100);
 
     @Override
-    @PostMapping(value="create")
-    public Object postStudent(@RequestBody Student student){
-        return studentRepository.postStudent(student);
+    @GetMapping(value="get-all")
+    public Object getAllStudents() {
+        Future<Object> future = executorService.submit(studentRepository::getAllStudents);
+        return getResultFromFuture(future);
     }
 
     @Override
     @DeleteMapping(value="delete-by-id")
     public Object deleteStudentByStudentId(@RequestBody Map<String,Integer> req){
-        return studentRepository.deleteStudentByStudentId(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.deleteStudentByStudentId(req));
+        return getResultFromFuture(future);
     }
 
     @Override
-    @GetMapping(value="get-all")
-    public Object getAllStudents() {
-        return studentRepository.getAllStudents();
+    @PostMapping(value="create")
+    public Object postStudent(@RequestBody Student student){
+        Future<Object> future = executorService.submit(() -> studentRepository.postStudent(student));
+        return getResultFromFuture(future);
     }
 
     @Override
     @GetMapping(value="get-class")
     public Object getStudentsFromClass(@RequestParam String classRoom){
-        return studentRepository.getStudentsFromClass(classRoom);
+        Future<Object> future = executorService.submit(() -> studentRepository.getStudentsFromClass(classRoom));
+        return getResultFromFuture(future);
     }
 
     @Override
     @GetMapping(value="get-student-id")
     public Object getStudentFromStudentId(@RequestParam Map<String, Integer> req){
-        return studentRepository.getStudentFromStudentId(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.getStudentFromStudentId(req));
+        return getResultFromFuture(future);
     }
 
     @Override
     @GetMapping(value="get-low-point")
     public Object getStudentsLowPoint(){
-        return studentRepository.getStudentsLowPoint();
+        Future<Object> future = executorService.submit(studentRepository::getStudentsLowPoint);
+        return getResultFromFuture(future);
     }
 
     @Override
     @PutMapping(value="update-class")
     public Object updateStudentClass(@RequestBody Req req){
-        return studentRepository.updateStudentClass(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.updateStudentClass(req));
+        return getResultFromFuture(future);
     }
 
     @Override
     @PutMapping(value="update-address")
     public Object updateStudentAddress(@RequestBody Req req) {
-        return studentRepository.updateStudentAddress(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.updateStudentAddress(req));
+        return getResultFromFuture(future);
     }
 
     @Override
     @PutMapping(value="update-point")
     public Object updateStudentPoint(@RequestBody DoubleReq req) {
-        return studentRepository.updateStudentPoint(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.updateStudentPoint(req));
+        return getResultFromFuture(future);
     }
 
     @Override
     @PutMapping(value="update-number")
     public Object updateStudentNumber(@RequestBody Req req) {
-        return studentRepository.updateStudentNumber(req);
+        Future<Object> future = executorService.submit(() -> studentRepository.updateStudentNumber(req));
+        return getResultFromFuture(future);
     }
 }
