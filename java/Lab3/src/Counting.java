1e2eb6ea18be8b102306685a133cef14e5e87324
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-11-19 16:23:27 +0700

    [Lab] Lab3 counting percent

--- java/Lab3/src/Counting.java
+++ java/Lab3/src/Counting.java
@@ -19,6 +19,16 @@ public class Counting {
 		return a;
 	}
 
+	/*
+	 * 3 trường hợp
+	 * + Ngoại lệ NegativeArraySizeException khi n nhập vào < 0 -> size của mảng ko đc bé hơn 0
+	 * + Ngoại lệ ArithmeticException khi tổng sum bằng 0: Nếu tổng sum của các phần tử trong mảng value bằng 0, 
+	 * phép chia (double) value[i] / sum sẽ gây ra một lỗi chia cho 0 và ném ra một ngoại lệ ArithmeticException.
+	 * + Ngoại lệ NullPointerException khi tham số value là null: Nếu tham số value được truyền vào là null, việc 
+	 * truy cập value.length trong câu lệnh int n = value.length sẽ gây ra một ngoại lệ NullPointerException.
+	 * + Ngoại lệ ArrayIndexOutOfBoundsException khi truy cập vượt quá chỉ số của mảng: Nếu chỉ số i trong vòng lặp
+	 * for (int i = 0; i < n; i++) vượt quá phạm vi chỉ số của mảng value, ví dụ như i = n hoặc hơn, thì việc truy cập value[i] sẽ gây ra ngoại lệ ArrayIndexOutOfBoundsException.
+	 */
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập số lượng phần tử: ");
@@ -32,8 +42,15 @@ public class Counting {
                     System.out.println("Không phải là số. Vui lòng nhập lại: ");
                     scanner.next();
                 }
+//            } catch (Exception e) {
+//				System.out.println("Exception happen");
+//				return;
+//			}
             } catch (NumberFormatException e) {
                 System.out.println("NumberFormatException");
+                return;
+            } catch (NegativeArraySizeException e) {
+                System.out.println("NegativeArraySizeException");
                 return;
             }
         }
