d7c6a90f77263fe53fd18ad72d9498bc433edac7
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-12-10 11:52:22 +0700

    [Lab] Variance and del data in file

--- java/Lab5/src/File/Main.java
+++ java/Lab5/src/File/Main.java
@@ -7,53 +7,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
-		try {
-			// đọc file
-			FileInputStream fis = new FileInputStream("src/File/input.txt");
-			InputStreamReader isr = new InputStreamReader(fis);
-			BufferedReader br = new BufferedReader(isr);
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("src/File/input.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
 
-			// parse hàng đầu thành số lượng hàng
-			int rFreq = Integer.parseInt(br.readLine());
+            int rowCount = Integer.parseInt(br.readLine());
 
-			// tạo mảng 2 chiều với data trong file sau đó
-			List<List<Integer>> a = new ArrayList<>();
-			for (int i = 0; i < rFreq; i++) {
-				String[] elements = br.readLine().split("\\s+"); // lấy data chia theo dấu cách
-				List<Integer> row = new ArrayList<>();
-				for (int j = 1; j < elements.length; j++) {
-					row.add(Integer.parseInt(elements[j]));
-				}
-				a.add(row);
-			}
+            List<List<Integer>> a = new ArrayList<>();
+            for (int i = 0; i < rowCount; i++) {
+                String[] ele = br.readLine().split("\\s+");
+                List<Integer> row = new ArrayList<>();
+                for (int j = 1; j < ele.length; j++) {
+                    row.add(Integer.parseInt(ele[j]));
+                }
+                a.add(row);
+            }
 
-			// lấy số truy vấn và xuất console các hàng truy vấn
-			int freq = Integer.parseInt(br.readLine());
-			for (int i = 0; i < freq; i++) {
-				String[] query = br.readLine().split("\\s+");
-				int row = Integer.parseInt(query[0]) - 1;
-				int start = Integer.parseInt(query[1]) - 1;
-				int end = Integer.parseInt(query[2]);
+            int q = Integer.parseInt(br.readLine());
+            for (int i = 0; i < q; i++) {
+                String[] query = br.readLine().split("\\s+");
+                int row = Integer.parseInt(query[0]) - 1;
+                int rowIndex = Integer.parseInt(query[1]) - 1;
+                int numOfele = Integer.parseInt(query[2]);
 
-				if (row >= 0 && row < a.size() && start >= 0
-						&& start < a.get(row).size()) {
-					for (int j = start; j < start + end; j++) {
-						if (j >= a.get(row).size()) {
-							System.out.print("ERROR");
-						} else {
-							System.out.print(a.get(row).get(j) + " ");
-						}
-					}
-					System.out.println();
-				} else {
-					System.out.println("ERROR");
-				}
-			}
+                if (row >= 0 && row < a.size()) {
+                    List<Integer> rows = a.get(row);
+                    if (rowIndex >= 0 && rowIndex < rows.size()) {
+                        int endIndex = Math.min(rowIndex + numOfele, rows.size());
+                        rows.subList(rowIndex, endIndex).clear(); 
+                        
+                        for (int j = 0; j < rows.size(); j++) {
+                            System.out.print(rows.get(j) + " ");
+                        }
+                        if(rows.size() == 0) {
+                            System.out.print("EMPTY");
+                        }
+                        System.out.println();
+                    } else {
+                        System.out.println("ERROR");
+                    }
+                } else {
+                    System.out.println("ERROR");
+                }
+            }
 
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- java/Lab5/src/Generic/MyMathOperation.java
+++ java/Lab5/src/Generic/MyMathOperation.java
@@ -4,49 +4,49 @@ import java.util.ArrayList;
 
 public class MyMathOperation<T extends Number> {
 
-    public double calculateStandardDeviation(ArrayList<T> arrayList) {
-        int N = arrayList.size();
+    public double calculateStandardDeviation(ArrayList<T> a) {
+        int N = a.size();
         if (N <= 0) {
             throw new IllegalArgumentException("Mảng rỗng");
         }
 
         double sum = 0;
-        double mean = calculateMean(arrayList);
+        double mean = calculateMean(a);
 
-        for (T element : arrayList) {
+        for (T element : a) {
             double value = element.doubleValue();
             sum += Math.pow(value - mean, 2);
         }
 
-        double variance = sum / N;
-        return Math.sqrt(variance);
+        double var = sum / N;
+        return Math.sqrt(var);
     }
 
-    private double calculateMean(ArrayList<T> arrayList) {
+    private double calculateMean(ArrayList<T> a) {
         double sum = 0;
-        for (T element : arrayList) {
+        for (T element : a) {
             sum += element.doubleValue();
         }
-        return sum / arrayList.size();
+        return sum / a.size();
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> intList = new ArrayList<>();
-        intList.add(10);
-        intList.add(20);
-        intList.add(30);
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(10);
+        a.add(20);
+        a.add(30);
 
-        MyMathOperation<Integer> intMathOperation = new MyMathOperation<>();
-        double intResult = intMathOperation.calculateStandardDeviation(intList);
-        System.out.println("Standard Deviation of Integer List: " + intResult);
+        MyMathOperation<Integer> inVal = new MyMathOperation<>();
+        double intResult = inVal.calculateStandardDeviation(a);
+        System.out.println("Độ lệch chuẩn của Integer List: " + intResult);
 
         ArrayList<Double> doubleList = new ArrayList<>();
         doubleList.add(10.5);
         doubleList.add(20.7);
         doubleList.add(30.2);
 
-        MyMathOperation<Double> doubleMathOperation = new MyMathOperation<>();
-        double doubleResult = doubleMathOperation.calculateStandardDeviation(doubleList);
-        System.out.println("Standard Deviation of Double List: " + doubleResult);
+        MyMathOperation<Double> doubleVal = new MyMathOperation<>();
+        double doubleResult = doubleVal.calculateStandardDeviation(doubleList);
+        System.out.println("Độ lệch chuẩn của Double List: " + doubleResult);
     }
 }
